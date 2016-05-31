2280ea89554a3215443fb0a29daa2528afad4151
juandent <juandent@mac.com>
2016-05-30 19:18:23 -0600

    FileSystemLib not compiling in Clang 3.7

---
+++ VS Builds/Filesystem/Filesystem_Tests/Filesystem_Tests/Filesystem_Tests.cpp
@@ -0,0 +1,19 @@
+// Filesystem_Tests.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#define PRINT_VER	wprintf( L"%s", _MSC_VER )
+
+/*
+#if _MSC_VER == 1900
+#error "Larger"
+#else
+#error "Smaller"
+#endif
+*/
+int main()
+{
+    return 0;
+}
+
